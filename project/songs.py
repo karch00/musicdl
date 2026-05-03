6156d70d74dcd8f5674a8df4f509692b406fad09
karch00 <karch00@proton.me>
2026-05-03 17:19:53 +0200

    Finished apply_metadata function

--- project/songs.py
+++ project/songs.py
@@ -3,6 +3,9 @@ import requests
 import re
 import yt_dlp
 import os
+from mutagen.flac import FLAC, Picture
+from mutagen.easyid3 import EasyID3
+from mutagen.id3 import ID3, APIC
 
 class FailedSongDownloadError:
     def __init__(self, message: str|None = None):
@@ -10,19 +13,35 @@ class FailedSongDownloadError:
 class FailedCoverDownloadError:
     def __init__(self, message: str|None = None):
         self.message = message
-class FailedMetadataError:
+class FailedMetadataApplyError:
     def __init__(self, message: str|None = None):
         self.message = message
 
 class Song:
     """
     Song class. Represents a song and its metadata.
+
+    ### Attributes:
+    - url (str): Song URL
+    - url_type (str): youtube-song or spotify-song
+    - title (str)
+    - artist (str)
+    - album (str)
+    - cover(str) : Album cover art URL or path
+    - genre(str)
+    - year(str)
+    - track(int)
+
+    ### Methods:
+    - download_song(parent_directory:str, song_format:Literal["mp3", "flac"], session:requests.Session)-> None | FailedSongDownloadError
+    - download_cover(parent_directory:str, session:requests.Session) -> None | FailedCoverDownloadError
+    - apply_metadata(parent_directory:str, song_format:Literal["mp3", "flac"]) -> None | FailedMetadataApplyError 
     """
     def __init__(
         self, 
         url: str,
         url_type: Literal[None, "youtube-song", "spotify-song"],
-        title: str|None = None, 
+        title: str, 
         artist: str|None = None, 
         album: str|None = None, 
         cover: str|None = None,
@@ -45,12 +64,12 @@ class Song:
         Gets metadata from the spotify link and returns the Title, Artist and Art album cover for
         more accurate search results from youtube.
 
-        Params:
-        session (requests.Session): Parent session
-        bearer_token (str): Spotify bearing token
+        ### Params:
+        - session (requests.Session): Parent session
+        - bearer_token (str): Spotify bearing token
 
-        Returns:
-        out (dict[str, str]): Title, Artist and art cover URL. None if request error
+        ### Returns:
+        - out (dict[str, str]): Title, Artist and art cover URL. None if request error
         """
         # Set headers and URL
         HEADERS = {
@@ -78,11 +97,11 @@ class Song:
         """
         Queries youtube and searches for the title to return the corresponding URL
 
-        Params:
-        title (str): The title to query
+        ### Params:
+        - title (str): The title to query
 
-        Returns:
-        out (str): Corresponding URL, None if query error
+        ### Returns:
+        - out (str): Corresponding URL, None if query error
         """
         # Init variables
         PATTERN=re.compile(r"\/watch\?v=.{11}")
@@ -100,19 +119,19 @@ class Song:
 
         return f"https://youtube.com{url_component}"
 
-    def __get_download_path__(self, base_path: str) -> str:
+    def __get_download_path_dir_tree__(self, parent_directory: str) -> str:
         """
         Returns the download path directory tree for song/cover to download
 
-        Params:
-        base_path (str): Parent path
+        ### Params:
+        - parent_directory (str): Parent path
 
-        Returns:
-        out (str): Full directory tree from base_path  
+        ### Returns:
+        - out (str): Full directory tree from parent_directory  
         """
         # /artist/album/song ; artist and album can be missing practically, but not recommended!
-        base_output_path = base_output_path.replace("\\", "/")
-        output_path = f"{base_output_path}{'/' if base_output_path[-1] != '/' else ''}"
+        parent_directory = parent_directory.replace("\\", "/")
+        output_path = f"{parent_directory}{'/' if parent_directory[-1] != '/' else ''}"
         if self.artist:
             output_path += f"/{self.artist.replace(' ', '_')}"
         if self.album:
@@ -122,18 +141,22 @@ class Song:
         
         return output_path
 
-    def download(self, base_output_path: str, song_format: Literal["mp3", "flac"], session: requests.Session, bearer_token: str|None = None) -> None|FailedSongDownloadError:
+    def download_song(self, parent_directory: str, song_format: Literal["mp3", "flac"], session: requests.Session, bearer_token: str|None = None) -> None|FailedSongDownloadError:
         """
         Downloads the song and its cover image onto the same 'Artist/Album/' directory
 
-        Params:
-        base_output_path (str): base output directory path
-        song_format (str): format: mp3 or flac
-        session (requests.Session): The session to make requests from
-        bearer_token (str|None): Spotify bearer token, None if no succesful spotify API bind or Spotify credentials not present
+        ### Params:
+        - parent_directory (str): base output directory path
+        - song_format (str): format: mp3 or flac
+        - session (requests.Session): The session to make requests from
+        - bearer_token (str|None): Spotify bearer token, None if no succesful spotify API bind or Spotify credentials not present
+
+        ### Returns:
+        error (FailedSongDownloadError): Error produced if any
         """
         # Get spotify metadata
         # Change artist and cover to queried if not set by custom metadata
+        # Query youtube for /watch?v= URL
         # Return FailedSongDownloadError if download failed for any reason
         if self.url_type == "spotify-song":
             if not bearer_token:
@@ -151,7 +174,7 @@ class Song:
                 return FailedSongDownloadError("Failed getting youtube URL: Request failed")
         
         # Create download directory inside parent output directory path
-        output_path = self.__get_download_path__(base_output_path)
+        output_path = self.__get_download_path_dir_tree__(parent_directory)
 
         if not os.path.exists(output_path):
             os.makedirs(output_path)
@@ -178,19 +201,22 @@ class Song:
         except Exception as e:
             return FailedSongDownloadError(f"Failed downloading song: {e}")
         
-    def download_cover(self, base_path: str, session: requests.Session) -> None|FailedCoverDownloadError:
+    def download_cover(self, parent_directory: str, session: requests.Session) -> None|FailedCoverDownloadError:
         """
         Downloads cover from self.cover URL. Meant to be called after download()
         Will return FailedCoverDownloadError if path does not exist since it means download() failed or was not called beforehand
 
-        Params:
-        base_path (str): Base path
-        session (requests.Session): The session to make requests from
+        ### Params:
+        - parent_directory (str): Base path
+        - session (requests.Session): The session to make requests from
+
+        ### Returns:
+        - error (FailedCoverDownloadError): Error produced, if any
         """ 
         # Download cover if present
         # Get request as stream, write by chunks into the cover path if does not exist already
         # Invalidates multiple <cover> tags inside the same album by design
-        output_path = self.__get_download_path__(base_path)
+        output_path = self.__get_download_path_dir_tree__(parent_directory)
         if not os.path.exists(output_path):
             return FailedCoverDownloadError("Song download path does not exist")
         
@@ -207,3 +233,77 @@ class Song:
         
         except Exception as e:
             return FailedCoverDownloadError("Failed fetching cover image")
+    
+    def apply_metadata(self, parent_directory: str, song_format: Literal["mp3", "flac"]) -> None|FailedMetadataApplyError:
+        """
+        Applies applicable metadata to the song
+
+        ### Params:
+        - parent_directory (str): Parent directory
+        - song_format (str): Song format
+
+        ### Returns:
+        - error (FailedMetadataApplyError): 
+        """
+        # Init variables
+        # Ensure valid song format, mp3 fallback
+        if song_format not in ["mp3", "flac"]:
+            song_format = "mp3"
+
+        # Check if parent dir and song paths exist
+        dir_path= self.__get_download_path_dir_tree__(parent_directory)
+        song_path = f"{dir_path}/{self.title.replace(' ', '_')}.{song_format}"
+        for path in [dir_path, song_path]:
+            if not os.path.exists(path):
+                return FailedMetadataApplyError(f"Failed metadata apply: File [{path}] does not exist")
+
+        # Change metadata to each attribute
+        # MAP[attribute : metatag]
+        ATTRIBUTES_MAP = {
+            "title": "title",
+            "artist": "artist",
+            "album": "album",
+            "genre": "genre",
+            "year": "date",
+            "track": "tracknumber"
+        }
+        if song_format == "flac":
+            audio = FLAC(song_path)
+        else:
+            audio = EasyID3(song_path)
+
+        for attribute, metatag in ATTRIBUTES_MAP.items():
+            audio[metatag] = self.__getattribute__(attribute)
+        audio.save()
+
+        # Set cover 
+        # Check if cover_path exists
+        cover_path = f"{dir_path}/cover"
+        if not os.path.exists(cover_path):
+            return FailedMetadataApplyError(f"Failed metadata cover apply: Cover [{path}] does not exist")
+        
+        try:
+            if self.cover:
+                if song_format == "flac":
+                    image = Picture()
+                    image.type = 3
+                    image.mime = "image/jpeg"
+                    image.data = open(cover_path, "rb").read()
+                    image.desc = "Cover art"
+                    audio.add_picture(image)
+                else:
+                    audio= ID3(song_path)
+                    audio.add(APIC(
+                        encoding = 3,
+                        mime = "image/jpeg",
+                        type = 3,
+                        desc = "Cover art",
+                        data = open(cover_path, "rb").read()
+                    ))
+                audio.save()
+        except Exception as e:
+            return FailedMetadataApplyError(f"Failed metadata cover apply: {e}")
+
+
+                    
+        
